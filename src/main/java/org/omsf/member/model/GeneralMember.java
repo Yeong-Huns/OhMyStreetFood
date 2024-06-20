b7186eea9f7048f385651c250748c3132825ec60
Yunbins <kig0632@naver.com>
2024-06-20 09:29:47 +0900

    Feat owner signup

--- src/main/java/org/omsf/member/model/GeneralMember.java
+++ src/main/java/org/omsf/member/model/GeneralMember.java
@@ -2,15 +2,29 @@ package org.omsf.member.model;
 
 import java.sql.Date;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
 public class GeneralMember extends Member {
+	@NotEmpty(message = "닉네임 입력은 필수입니다.")
+	@Size(min = 2)
+	private String nickName;
 	
-	@Builder
-	public GeneralMember(String username, String nickName, String password, String memberType,
-			String loginType, Date createdAt, Date modifiedAt) {
-		super(username, nickName, password, memberType, loginType, createdAt, modifiedAt);
-	}
-
+    @Builder
+    public GeneralMember(String username, String nickName, String password,
+                         String loginType, Date createdAt, Date modifiedAt) {
+        super(username, password, "일반", loginType, createdAt, modifiedAt);
+        this.nickName=nickName;
+    }
+    
 }
