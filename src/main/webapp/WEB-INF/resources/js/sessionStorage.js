b5ad50b21c6b1756406e84491365f82f21937945
leeeejsp <141730566+leeeejsp@users.noreply.github.com>
2024-06-28 18:05:37 +0900

    Feat log_store

--- src/main/webapp/WEB-INF/resources/js/sessionStorage.js
+++ src/main/webapp/WEB-INF/resources/js/sessionStorage.js
@@ -1,6 +1,6 @@
-/**
- * 
- */
+///**
+// * 
+// */
 
 async function getCurrentLocationAsync() {
 	if (!navigator.geolocation) {
@@ -62,4 +62,60 @@ async function getLatitudeAndLongitude(){
 	
 })();
 
-
+//async function getCurrentLocationAsync() {
+//    if (!navigator.geolocation) {
+//        throw new Error("Geolocation is not supported by this browser.");
+//    }
+//
+//    return new Promise((resolve, reject) => {
+//        navigator.geolocation.getCurrentPosition(resolve, reject);
+//    });
+//}
+//
+//async function displayLocation() {
+//    try {
+//        const position = await getCurrentLocationAsync();
+//        const gps_lat = position.coords.latitude;
+//        const gps_lng = position.coords.longitude;
+//        console.log(`Latitude: ${gps_lat}, Longitude: ${gps_lng}`);
+//        return { gps_lat, gps_lng }; // 위치 정보를 반환
+//    } catch (error) {
+//        console.error(error.message);
+//        return { gps_lat: defaultLat, gps_lng: defaultLon }; // 오류 발생 시 기본 위치 반환
+//    }
+//}
+//
+//async function getLatitudeAndLongitude() {
+//    if (sessionStorage.getItem('latitude') === null || 
+//        sessionStorage.getItem('longitude') === null) {
+//        console.log("세션 스토리지에 값이 없어 다시 위치 정보를 불러옵니다.");
+//        const location = await displayLocation();
+//
+//        sessionStorage.setItem('latitude', location.gps_lat);
+//        sessionStorage.setItem('longitude', location.gps_lng);
+//    }
+//
+//    const latitude = sessionStorage.getItem('latitude');
+//    const longitude = sessionStorage.getItem('longitude');
+//
+//    console.log("현재 위도 값 : " + latitude);
+//    console.log("현재 경도 값 : " + longitude);
+//
+//    const address = {
+//        latitude: latitude,
+//        longitude: longitude
+//    };
+//    const queryStr = new URLSearchParams(address).toString();
+//
+//    const distanceElement = document.getElementById("distance");
+//    if (distanceElement) {
+//        const nowHref = distanceElement.href;
+//        distanceElement.href = nowHref + '&' + queryStr;
+//    } else {
+//        console.error("요소를 찾을 수 없습니다.");
+//    }
+//}
+//
+//document.addEventListener("DOMContentLoaded", async () => {
+//    await getLatitudeAndLongitude();
+//});
